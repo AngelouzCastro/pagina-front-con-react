d557ed8ff6cefed5d548ab258d85fa88cce7d0ec
darkangel9912 <am43591@gmail.com>
2022-07-20 23:02:05 -0500

    pagina de peliculas con react front v1

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import PageWrapper from "./view/components/PageWrapper";
+import Pelicula from "./view/components/Pelicula";
+import peliculasJson from "./view/json/peliculas.json"
+import Paginacion from "./view/components/paginacion";
+import {useState} from "react";
+
+function App() {
+
+    const [paginaActual, setPaginaActual] = useState(1);
+    const TOTAL_POR_PAGINA = 7;
+
+    let peliculas = peliculasJson;
+
+    const cargarPeliculas = () => {
+        peliculas = peliculas.slice(
+            (paginaActual - 1) * TOTAL_POR_PAGINA,
+            paginaActual * TOTAL_POR_PAGINA
+        );
+    }
+
+    const getTotalPaginas = () => {
+        let cantidadTotalDePeliculas = peliculasJson.length;
+        return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
+    }
+
+    cargarPeliculas();
+
+    return (
+        <PageWrapper>
+
+            {peliculas.map(pelicula =>
+                <Pelicula titulo={pelicula.titulo}
+                          calificacion={pelicula.calificacion}
+                          director={pelicula.director}
+                          actores={pelicula.actores}
+                          fecha={pelicula.fecha}
+                          duracion={pelicula.duracion}
+                          img={pelicula.img}>
+                    {pelicula.descripcion}
+                </Pelicula>
+            )}
+
+            <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
+                setPaginaActual(pagina)
+            }} />
+
+        </PageWrapper>
+    );
+}
+
+export default App;
